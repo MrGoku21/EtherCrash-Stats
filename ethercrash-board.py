831c2215e4d37f0cbf68f4587ea32bc604159bf0
Isaac <47527704+MrGoku21@users.noreply.github.com>
2020-11-03 12:48:54 +0100

    Update
    Fixed a checker where if a user isn't on ethercrash, the program will return a error message explaining that the specific user isn't a valid one.

--- ethercrash-board.py
+++ ethercrash-board.py
@@ -6,16 +6,21 @@ import time
 id = input("Username: ")
 print("\n")
 
-page = requests.get(f'https://www.ethercrash.io/user/{id}')
-soup = BeautifulSoup(page.text, 'html.parser')
-i = soup.find('div', id_="container")
+try:
+    page = requests.get(f'https://www.ethercrash.io/user/{id}')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    i = soup.find('div', id_="container")
 
-for i in soup():
-    games = soup.select('h4', class_="mb-0 card-title")[1].get_text()
-    rank = soup.select('h4', class_="mb-0 card-title")[2].get_text()
-    profit = soup.select('h4', class_="mb-0 card-title")[3].get_text()
-    net = soup.select('h4', class_="mb-0 card-title")[4].get_text()
+    for i in soup():
+        games = soup.select('h4', class_="mb-0 card-title")[1].get_text()
+        rank = soup.select('h4', class_="mb-0 card-title")[2].get_text()
+        profit = soup.select('h4', class_="mb-0 card-title")[3].get_text()
+        net = soup.select('h4', class_="mb-0 card-title")[4].get_text()
+        
+    print(f"User: {id}\n\nGames: {games}\nRank: {rank}\nGross Profit: {profit}\nNet Profit {net}")  
+
+except IndexError:
+    print(f"{id} is most likely not a valid user!")
 
 
-print(f"{id}\n\nGames: {games}\n\nRank: {rank}\n\nGross Profit: {profit}\n\nNet Profit {net}")
 
