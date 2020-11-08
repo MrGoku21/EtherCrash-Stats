3cec840bb63352a7cee7bedd886361cdbb544b32
C4451 <47527704+MrGoku21@users.noreply.github.com>
2020-11-08 15:49:16 +0100

    Update ethercrash-board.py
    Added def functions for more polish design

--- ethercrash-board.py
+++ ethercrash-board.py
@@ -3,24 +3,24 @@ import re
 import requests
 import time
 
-id = input("Username: ")
-print("\n")
+id = input("User: ")
 
-try:
-    page = requests.get(f'https://www.ethercrash.io/user/{id}')
-    soup = BeautifulSoup(page.text, 'html.parser')
-    i = soup.find('div', id_="container")
+def algorithm(id):
+    try:
+        page = requests.get(f'https://www.ethercrash.io/user/{id}')
+        soup = BeautifulSoup(page.text, 'html.parser')
+        i = soup.find('div', id_="container")
 
-    for i in soup():
-        games = soup.select('h4', class_="mb-0 card-title")[1].get_text()
-        rank = soup.select('h4', class_="mb-0 card-title")[2].get_text()
-        profit = soup.select('h4', class_="mb-0 card-title")[3].get_text()
-        net = soup.select('h4', class_="mb-0 card-title")[4].get_text()
-        
-    print(f"User: {id}\n\nGames: {games}\nRank: {rank}\nGross Profit: {profit}\nNet Profit {net}")  
+        for i in soup():
+            games = soup.select('h4', class_="mb-0 card-title")[1].get_text()
+            rank = soup.select('h4', class_="mb-0 card-title")[2].get_text()
+            profit = soup.select('h4', class_="mb-0 card-title")[3].get_text()
+            net = soup.select('h4', class_="mb-0 card-title")[4].get_text()
+            
+        print(f"\n\nGames: {games}\nRank: {rank}\nGross Profit: {profit}\nNet Profit {net}")  
 
-except IndexError:
-    print(f"{id} is most likely not a valid user!")
-
+    except IndexError:
+        print(f"{id} is most likely not a valid user!")
+algorithm(id)
 
 
